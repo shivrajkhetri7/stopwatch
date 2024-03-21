67e4ec3d72cc5f914f702b573f1189546ef5c2b9
Shivraj Ramesh Khetri <59004184+shivrajkhetri7@users.noreply.github.com>
2024-03-21 17:48:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [timer, setTimer] = useState(0);
+  const [pause, setPause] = useState(false);
+  const [start, setStart] = useState(false);
+
+  const handlePause = () => {
+    setPause(!pause);
+  };
+
+  /**
+   *  @function handleStart
+   *  @description this function uses for the start the stop watch
+   */
+  const handleStart = () => {
+    setStart(true);
+    if (pause) {
+      setPause(false);
+    }
+  };
+
+  // TODO this function is for stoping the stopwatch
+  const handleStop = () => {
+    setStart(false);
+    setTimer(0);
+  };
+
+  useEffect(() => {
+    let stopWatchTime;
+    if (start && !pause) {
+      stopWatchTime = setInterval(() => {
+        setTimer((prev) => prev + 10);
+      }, 10);
+    }
+    return () => clearInterval(stopWatchTime);
+  },[start,pause]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="timer">
+          <span className="digits">
+            {("0" + Math.floor((timer / 60000) % 60)).slice(-2)}:
+          </span>
+          <span className="digits">
+            {("0" + Math.floor((timer / 1000) % 60)).slice(-2)}.
+          </span>
+          <span className="digits mili-sec">
+            {("0" + ((timer / 10) % 100)).slice(-2)}
+          </span>
+        </div>
+        <div className="lowe-container">
+          <button className="btn success" onClick={handleStart}>
+            Start
+          </button>
+          <button className="btn warning" onClick={handlePause}>
+            Pause
+          </button>
+          <button className="btn error" onClick={handleStop}>
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
